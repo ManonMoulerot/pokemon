3929126d2801bda6e3e4eb935729e420806e2f18
manon.moulerot.rdl@gmail.com <ManonMoulerot>
2020-03-17 11:09:08 +0100

    essai2

--- models/form.php
+++ models/form.php
@@ -56,6 +56,42 @@ class Formulaire
     public function submit() // Une méthode qui un submit
     {
        return '<input type="submit" value="envoyer">';
+    }
+    
+    public function inputFichier() // Une méthode qui pop un input file et son bouton
+    {
+        echo '<input type="hidden" name="dossier" value="image"/>';
+        echo '  <input type="file" name="file" id="file" />';
+        echo '  <input class="button" type="submit" />';
     }    
+
+    public function fichier($repertoireDestination,$nomFichier,$nouveauNom) //qui envoi le fichier dans le chemin données avec son nouveau nom
+    {
+
+                $nomFichier=$_FILES['file']['name'];
+                $nomDestination = $nouveauNom.".png";
+                $nomOrigine = $_FILES['file']['name'];
+                $elementsChemin = pathinfo($nomOrigine);
+                $extensionFichier = $elementsChemin['extension'];
+                $extensionsAutorisees = array("png");
+                if (!(in_array($extensionFichier, $extensionsAutorisees))) {
+                    echo "Le fichier n'a pas l'extension attendue";
+                } else {
+    
+                if (move_uploaded_file($_FILES["file"]["tmp_name"], 
+                                                $repertoireDestination.$nomDestination)) {
+                    echo "Le fichier temporaire ".$_FILES["file"]["tmp_name"].
+                            " a été déplacé vers ".$repertoireDestination.$nomDestination;
+                } else {
+                    echo "Le fichier n'a pas été uploadé (trop gros ?) ou ".
+                            "Le déplacement du fichier temporaire a échoué".
+                            " vérifiez l'existence du répertoire ".$repertoireDestination;
+                }
+
+    }
+
+}  
+}  
+
 }  
 
